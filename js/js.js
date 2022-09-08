eea64f42ebfd2c34894ab255ead449b876b63cbf
Kamshory, MT <kamshory@yahoo.com>
2022-09-08 14:22:20 +0700

    Stop another audio before playing

--- js/js.js
+++ js/js.js
@@ -5,6 +5,7 @@ if (document.location.search.match(/type=embed/gi)) {
 $(document).ready(function (e) {
 
 });
+var lastPlayed = '';
 $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
@@ -22,6 +23,7 @@ $(document).ready(function () {
 
 });
 function endAudio(obj) {
+    $(obj).attr('data-playing', 'false');
     var parent = $(obj).closest('.ayat-item');
     var uncle = parent.next();
     if (uncle.length > 0) {
@@ -34,6 +36,18 @@ function endAudio(obj) {
 
 function playAudio(obj)
 {
+    var played = $('audio[data-playing="true"]');
+    if(played.length)
+    {
+        played.each(function(e){
+            $(this).attr('data-playing', 'false');
+            $(this).trigger('pause');
+            $(this).get(0).currentTime = 0;
+        });
+       
+    }
+    $(obj).attr('data-playing', 'true');
+    lastPlayed = $(obj).attr('data-ayat-key');
     var parent = $(obj).closest('.ayat-item');
     var scrollPosition = parent.offset().top;
     $("html, body").animate({ scrollTop: scrollPosition });
